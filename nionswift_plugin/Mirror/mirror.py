e10a262a7f17cb70ee7a8eb2491e831e2aea5c99
yvesauad <yvesauad@gmail.com>
2020-07-25 09:38:54 +0200

    v3.7 Start Mirror Control using Zaber Motor

---
+++ nionswift_plugin/Mirror/mirror.py
@@ -0,0 +1,59 @@
+import serial
+import sys
+import logging
+import time
+import threading
+import numpy
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+
+__author__ = "Yves Auad"
+
+
+def _isPython3():
+    return sys.version_info[0] >= 3
+
+
+def SENDMYMESSAGEFUNC(sendmessagefunc):
+    return sendmessagefunc
+
+
+class Mirror:
+
+    def __init__(self, sendmessage):
+        self.sendmessage = sendmessage
+        self.ser = serial.Serial()
+        self.ser.baudrate = 9600
+        self.ser.port = 'COM7'
+        self.ser.parity = serial.PARITY_NONE
+        self.ser.stopbits = serial.STOPBITS_ONE
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.timeout = 0.2
+
+        try:
+            if not self.ser.is_open:
+                self.ser.open()
+                time.sleep(0.1)
+        except:
+            self.sendmessage(1)
+
+        self.ser.readline()
+
+    def pos_to_bytes(self, pos):
+        rem = pos
+        val = numpy.zeros(4, dtype=int)
+        for j in range(4):  # 4 bytes
+            val[j] = rem % 256
+            rem = rem - val[j]
+            rem = rem / 256
+        return val
+
+    def set_val(self, motor, value):
+        try:
+            byt = self.pos_to_bytes(value)
+            message = [motor, 20, byt[0], byt[1], byt[2], byt[3]]
+            byt_array = bytearray(message)
+            self.ser.write(byt_array)
+            self.ser.read(6)
+        except:
+            self.sendmessage(2)
--- nionswift_plugin/OptSpec/spec_vi.py
+++ nionswift_plugin/OptSpec/spec_vi.py
@@ -1,43 +1,54 @@
 import sys
 import time
 import numpy
+import os
+import json
+
+abs_path = os.path.abspath(os.path.join((__file__+"/../../"), 'global_settings.json'))
+with open(abs_path) as savfile:
+    settings = json.load(savfile)
+LP_MM = settings["SPECTROMETER"]["GRATINGS"]["LP_MM"]
 
 __author__ = "Yves Auad"
+
 
 def _isPython3():
     return sys.version_info[0] >= 3
 
+
 def SENDMYMESSAGEFUNC(sendmessagefunc):
     return sendmessagefunc
+
 
 class OptSpectrometer:
 
     def __init__(self, sendmessage):
-        self.sendmessage=sendmessage
+        self.sendmessage = sendmessage
         self.wavelength = 550.
         self.now_grating = 0
         self.entrance_slit = 3000
         self.exit_slit = 3000
         self.which_slit = 1
+        self.lp_mm=LP_MM
 
     def set_grating(self, value):
-        a=abs(numpy.random.randn(1)[0]*5)
-        time.sleep(a+1)
+        a = abs(numpy.random.randn(1)[0] * 5)
+        time.sleep(a + 1)
         self.sendmessage(2)
 
     def set_wavelength(self, value):
-        a=abs(numpy.random.randn(1)[0]*5)
-        time.sleep(a+1)
+        a = abs(numpy.random.randn(1)[0] * 5)
+        time.sleep(a + 1)
         self.sendmessage(3)
 
     def set_entrance(self, value):
-        a=abs(numpy.random.randn(1)[0]*5)
-        time.sleep(a+1)
+        a = abs(numpy.random.randn(1)[0] * 5)
+        time.sleep(a + 1)
         self.sendmessage(4)
 
     def set_exit(self, value):
-        a=abs(numpy.random.randn(1)[0]*5)
-        time.sleep(a+1)
+        a = abs(numpy.random.randn(1)[0] * 5)
+        time.sleep(a + 1)
         self.sendmessage(5)
 
     def set_which(self, value):
