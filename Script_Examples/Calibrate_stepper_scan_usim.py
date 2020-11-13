48d59c0df4630ddfa092927385cb9da6f7eb045d
yvesauad <yvesauad@gmail.com>
2020-11-13 11:51:08 +0100

    v4.1.6: Calib in usim done. Will begin tests in real machine.

--- Script_Examples/Calibrate_stepper_scan_usim.py
+++ Script_Examples/Calibrate_stepper_scan_usim.py
@@ -4,6 +4,7 @@ import time
 from nion.typeshed import API_1_0 as API
 from nion.typeshed import UI_1_0 as UI
 from nion.utils import Geometry
+from nion.swift.model import Graphics
 
 api = api_broker.get_api(API.version, UI.version)
 
@@ -11,7 +12,7 @@ scan = HardwareSource.HardwareSourceManager().get_hardware_source_for_hardware_s
 stage = HardwareSource.HardwareSourceManager().get_instrument_by_id("stage_controller")
 my_inst = HardwareSource.HardwareSourceManager().get_instrument_by_id("usim_stem_controller")
 
-my_inst.SetVal2D("stage_position_m", Geometry.FloatPoint(y=1e-9, x=1e-8))
+my_inst.SetVal2D("stage_position_m", Geometry.FloatPoint(y=0e-9, x=0e-8))
 
 '''
 xdata = numpy.random.randn(10, 10, 1024)
@@ -28,10 +29,8 @@ data_item = api.library.create_data_item_from_data_and_metadata(si_xdata)
 '''
 
 det = scan.grab_next_to_start()[0]
-#data_item = api.library.create_data_item_from_data_and_metadata(det)
-#data_item.title = 'First Image'
 
-pts = 4
+pts = 8
 sub_region = 0.25
 
 xarray = numpy.linspace(-sub_region, sub_region, pts)
@@ -47,6 +46,8 @@ y_samp = (fov)/(ia[1])
 initial_stage_x, initial_stage_y = my_inst.GetVal2D("stage_position_m")
 initial_probe_x, initial_probe_y = scan.probe_position
 
+
+
 if abs(initial_probe_x-0.5)>0.01 or abs(initial_probe_y-0.5)>0.01:
     raise Exception("***MECHANICAL SPECTRA***: Put probe close to (0.5, 0.5). 1% tolerance allowed. ")
 
@@ -59,28 +60,30 @@ print(f'Image area (pixels): {(ia[0])} and {(ia[1])}')
 print(f'Pixels per step: {(ia[0])/pts} and {(ia[1])/pts}')
 print(f'initial probe position is {initial_probe_x} and {initial_probe_y}')
 
-#data = list()
-xdata = numpy.zeros((pts, pts, 512, 512))
+xdata = numpy.zeros((pts, pts, 256, 256))
 
+def highlight_data(data, index, shape, w, value, sen):
+    x, y = index
+    cx = (x+0.5)*shape[0]
+    cy = (y+0.5)*shape[1] if sen==1 else shape[1] - (y+0.5)*shape[1]
+    data[int(cy-w):int(cy+w), int(cx-w):int(cx+w)] = value
 
-sen = -1
+sen = 1
 for xi, x in enumerate(xarray):
-    #stage.x_pos_f = initial_stage_x + x*fov*1e8 #You put 400 to have 4 microns in this property here
     sen = sen * -1
     for yi, y in enumerate(yarray):
-        stage_x, stage_y = my_inst.GetVal2D("stage_position_m")
-        #stage.y_pos_f = initial_stage_y + y*fov*1e8*sen
+        my_inst.SetVal2D("stage_position_m", Geometry.FloatPoint(y=y*fov*1e-9*sen, x=x*fov*1e-9))
         im = scan.grab_next_to_start()
-        #data.append([stage_x, stage_y, im[0]])
-        xdata[xi, yi] = im[0].data
-        #scan.scan_device.probe_pos = ((x+0.5), (y+0.5))
+        highlight_data(im[0].data, (x, y), (ia[0], ia[1]), 1, 5, sen)
+        point = Geometry.IntPoint.make((0, 0))
+        if sen==1:
+            xdata[xi, yi] = im[0].data
+        else:
+            xdata[xi, pts-1-yi] = im[0].data
 
 si_data_descriptor = api.create_data_descriptor(is_sequence=False, collection_dimension_count=2, datum_dimension_count=2)
 si_xdata = api.create_data_and_metadata(xdata, data_descriptor=si_data_descriptor)
 data_item = api.library.create_data_item_from_data_and_metadata(si_xdata)
 
-stage.x_pos_f = initial_stage_x
-stage.y_pos_f = initial_stage_y
-cam_eire.stop_playing()
-#scan.scan_device.probe_pos = (initial_probe_x, initial_probe_y)
+my_inst.SetVal2D("stage_position_m", Geometry.FloatPoint(y=0, x=0))
 
