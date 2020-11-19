6c05e536cb43ee0d7fa37cada067a6401f18aa4f
yvesauad <yvesauad@gmail.com>
2020-11-19 18:46:52 +0100

    v4.3.2: First working version of the analysis. CL spikes cant be controlled

--- Script_Examples/Mechanical_spectra_analysis.py
+++ Script_Examples/Mechanical_spectra_analysis.py
@@ -4,7 +4,7 @@ from nion.typeshed import API_1_0 as API
 from nion.typeshed import UI_1_0 as UI
 
 api = api_broker.get_api(API.version, UI.version)  # type: API
-DI = api.library.get_data_item_by_uuid(uuid.UUID("de1e1c89-55d1-43c4-ba2f-eeb5a0d6446c"))
+DI = api.library.get_data_item_by_uuid(uuid.UUID("0900f18a-e8f7-40a8-ab87-8f5b70a8a771"))
 
 print(f'Data Item title is {DI.title}')
 print(f'Data item has shape of {DI.data.shape}')
@@ -16,7 +16,15 @@ for xpos in range(x):
     print('Done '+format(100*xpos/x, '.1f')+' %')
     for ypos in range(y):
         spec = DI.data[xpos, ypos]
-        minval[xpos, ypos], maxval[xpos, ypos] = min(spec), max(spec)
+        minval[xpos, ypos], maxval[xpos, ypos] = min(spec[750:850]), max(spec[750:850])
 
-print(minval)
-print(numpy.average(minval))
+
+maxval = numpy.subtract(maxval, minval)
+maxval = numpy.divide(maxval, maxval.max())
+
+
+dimensional_calibrations = DI.dimensional_calibrations[:-1]
+xdata = api.create_data_and_metadata(maxval,
+                                     dimensional_calibrations=dimensional_calibrations)
+data_item = api.library.create_data_item_from_data_and_metadata(xdata)
+data_item.title = '**SCRIPTED**'+DI.title
