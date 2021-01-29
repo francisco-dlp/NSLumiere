5cfa4ce4e55fa09f5fe3741f2c47856d07d6e6c0
yvesauad <yvesauad@gmail.com>
2021-01-29 09:20:13 +0100

    v4.9.0. Abstract class for probe

---
+++ nionswift_plugin/lenses/lens_controller.py
@@ -0,0 +1,40 @@
+import sys
+import logging
+import time
+import threading
+import numpy
+import abc
+
+__author__ = "Yves Auad"
+
+class LensesController(abc.ABC):
+
+    def __init__(self, sendmessage):
+        self.sendmessage = sendmessage
+        self.lock = threading.Lock()
+
+
+    @abc.abstractmethod
+    def query(self, which):
+        """Query Lens"""
+
+    @abc.abstractmethod
+    def set_val(self, val, which):
+        """Set Lens Value"""
+
+    def wobbler_loop(self, current, intensity, frequency, which):
+        self.wobbler_thread = threading.currentThread()
+        sens = 1
+        while getattr(self.wobbler_thread, "do_run", True):
+            sens = sens * -1
+            if getattr(self.wobbler_thread, "do_run", True): time.sleep(1. / frequency)
+            self.set_val(current + sens * intensity, which)
+            if getattr(self.wobbler_thread, "do_run", True): time.sleep(1. / frequency)
+            self.set_val(current, which)
+
+    def wobbler_on(self, current, intensity, frequency, which):
+        self.wobbler_thread = threading.Thread(target=self.wobbler_loop, args=(current, intensity, frequency, which), )
+        self.wobbler_thread.start()
+
+    def wobbler_off(self):
+        self.wobbler_thread.do_run = False
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="Lumiere Ultra Combo",
-    version="4.8.41",
+    version="4.9.0",
     author="Yves Auad",
     description="Lenses, EELS_Spec, Apertures,  Instrument",
     url="https://github.com/yvesauad/swift_lumiere",
