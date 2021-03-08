a317d3a664b91709492b8074a33aa7ec84b1c7dc
yvesauad <yvesauad@gmail.com>
2021-03-08 18:15:32 +0100

    v5.0.8. General purpose script to study scan and camera

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="Lumiere Ultra Combo",
-    version="5.0.7",
+    version="5.0.8",
     author="Yves Auad",
     description="Lenses, EELS_Spec, Apertures,  Instrument",
     url="https://github.com/yvesauad/swift_lumiere",
