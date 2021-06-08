f647820ba06cffc11b21193c81e9b51f895058e2
yvesauad <yvesauad@gmail.com>
2021-06-08 15:42:07 +0200

    v5.5.1. Putting nionswift in VGCold P2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OrsayLumiere",
-    version="5.5.0",
+    version="5.5.1",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
