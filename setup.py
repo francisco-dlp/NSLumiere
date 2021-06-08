dc05bfe5cca66bc1e194c760a8f9456e3cfa7341
yvesauad <yvesauad@gmail.com>
2021-06-08 15:50:56 +0200

    v5.5.3. New requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="OrsayLumiere",
-    version="5.5.2",
+    version="5.5.3",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
     packages=find_packages(
         include=['nionswift_plugin.OptSpec',]),
     package_dir={"": "."},
-    python_requires='~=3.9.5',
-    install_requires=["pyserial>=3.5", "requests>=2.25.1"]
+    python_requires='>=3.8.5',
+    install_requires=["pyserial>=3.5", "requests>=2.25.1", "nionswift_usim>=0.3.0"]
 )
