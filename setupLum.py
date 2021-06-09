7086fd8f7210cb5c3adf08d92da8774f1e6966eb
yvesauad <yvesauad@gmail.com>
2021-06-09 08:52:12 +0200

    v5.6.0. Working on a multiplatform microscope

--- setupLum.py
+++ setupLum.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="OrsayLumiere",
-    version="5.5.5",
+    version="5.6.0",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
-    packages=find_packages(
-        include=['nionswift_plugin.OptSpec',]),
-    package_dir={"": "."},
+    packages=['nionswift_plugin', 'nionswift_plugin.diaf', 'nionswift_plugin.EELS_spec',
+              'nionswift_plugin.IVG', 'nionswift_plugin.lenses',
+              'nionswift_plugin.OptSpec', 'nionswift_plugin.stage'],
     python_requires='>=3.8.5',
     install_requires=["pyserial>=3.5", "requests>=2.25.1", "nionswift-usim>=0.3.0"]
 )
