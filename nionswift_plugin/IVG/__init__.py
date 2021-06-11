bc308e984e0a8083d3a7c0d028ee999587d6fa28
yvesauad <yvesauad@gmail.com>
2021-06-11 13:18:38 +0200

    v5.8.2. Test on VG Lumiere. ok.

--- nionswift_plugin/IVG/__init__.py
+++ nionswift_plugin/IVG/__init__.py
@@ -1,20 +1,11 @@
 from nion.utils import Registry
-import os
-import json
-
-abs_path = os.path.join(os.path.dirname(__file__), '../aux_files/config/global_settings.json')
-with open(abs_path) as savfile:
-    settings = json.load(savfile)
-
-DEBUG_SCAN = settings["IVG"]["DEBUG_SCAN"]
 
 from . import ivg_inst
 from . import ivg_panel
 from . import ivg_spim_panel
 
 from .camera import VGCameraPanel, VGCameraYves
-if not DEBUG_SCAN:
-    from .scan import VGScanYves
+from .scan import VGScanYves
 
 def run():
 
@@ -23,8 +14,8 @@ def run():
     Registry.register_component(instrument, {"instrument_controller", "stem_controller"})
 
     ivg_panel.run(instrument)
-    if not DEBUG_SCAN: ivg_spim_panel.run(instrument)
-    if not DEBUG_SCAN: VGScanYves.run(instrument)
+    ivg_spim_panel.run(instrument)
+    VGScanYves.run(instrument)
     VGCameraYves.run(instrument)
     VGCameraPanel.run()
 
--- setupLum.py
+++ setupLum.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrsayLumiere",
-    version="5.8.1",
+    version="5.8.2",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
