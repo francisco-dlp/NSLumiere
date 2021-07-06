11a2e4e868fd3a75e6b4eab0bfc9b9eca07791d0
kociak <mathieu.kociak@u-psud.fr>
2021-07-06 15:24:24 +0200

    v5.11.8 Listing TP3 in VG Cold

--- setupLum.py
+++ setupLum.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrsayLumiere",
-    version="5.11.7",
+    version="5.11.8",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
