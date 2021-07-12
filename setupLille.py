7a1c50b69f06777511e3d7bb0f2d9d7d8f0a6704
yvesauad <yvesauad@gmail.com>
2021-07-12 12:05:07 +0200

    v5.12.0. Minor fix in spectrometer

--- setupLille.py
+++ setupLille.py
@@ -1,23 +1,19 @@
 from setuptools import setup
 
 setup(
-    name="OrsayLumiere",
-    version="5.12.0",
+    name="OrsayLille",
+    version="LumiereBranch",
     author="Yves Auad",
-    description="Set of tools to run a VG Microscope in Nionswift",
+    description="Set of tools to run VG Cold Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
-    packages=['nionswift_plugin.EELS_spec', 'nionswift_plugin.diaf',
-              'nionswift_plugin.IVG', 'nionswift_plugin.IVG.virtual_instruments',
+    packages=['nionswift_plugin.EELS_spec', 'nionswift_plugin.IVG', 'nionswift_plugin.IVG.virtual_instruments',
               'nionswift_plugin.IVG.tp3', 'nionswift_plugin.IVG.camera',
-              'nionswift_plugin.IVG.scan', 'nionswift_plugin.lenses',
-              'nionswift_plugin.OptSpec', 'nionswift_plugin.stage'],
+              'nionswift_plugin.IVG.scan'],
     python_requires='>=3.8.5',
     data_files=[('nionswift_plugin/aux_files/config', [
-        'nionswift_plugin/aux_files/config/Lumiere/global_settings.json',
-        'nionswift_plugin/aux_files/config/Lumiere/diafs_settings.json',
+        'nionswift_plugin/aux_files/config/Cold/global_settings.json',
         'nionswift_plugin/aux_files/config/Lumiere/eels_settings.json',
-        'nionswift_plugin/aux_files/config/Lumiere/lenses_settings.json',
-        'nionswift_plugin/aux_files/config/Lumiere/Orsay_cameras_list.json',
+        'nionswift_plugin/aux_files/config/Cold/Orsay_cameras_list.json',
 
     ]), ('nionswift_plugin/aux_files/DLLs', [
         'nionswift_plugin/aux_files/DLLs/Cameras.dll',
@@ -30,8 +26,6 @@ setup(
         'nionswift_plugin/aux_files/DLLs/delib64.dll',
         'nionswift_plugin/aux_files/DLLs/SpectroCL.dll',
         'nionswift_plugin/aux_files/DLLs/AttoClient.dll',
-    ]), ('swift_rust/target/release', [
-        'swift_rust/target/release/rust2swift.pyd'
     ]
 
     )],
