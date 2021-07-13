f86ea32ccaa87488c81454c06429f9a0b4d7b696
yvesauad <yvesauad@gmail.com>
2021-07-13 15:24:01 +0200

    v5.12.11. Camera simulation is false

--- setupLum.py
+++ setupLum.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrsayLumiere",
-    version="5.12.10",
+    version="5.12.11",
     author="Yves Auad",
     description="Set of tools to run a VG Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
