4417b2262ab401218dadc77e4103f27514b1e466
yvesauad <yvesauad@gmail.com>
2023-04-13 15:50:58 +0200

    v8.0.4: update after timepix tests

--- setupCold.py
+++ setupCold.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrsayCold",
-    version="8.0.3",
+    version="8.0.4",
     author="Yves Auad",
     description="Set of tools to run VG Cold Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
