71dc6468a0f567a769eeb273495f1607f863c564
yvesauad <yvesauad@gmail.com>
2023-07-25 09:17:16 +0200

    v10.1.5: detecting automatically if is chromaTEM or not. Debug version is working on my computer

--- setupCold.py
+++ setupCold.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="OrsayCold",
-    version="10.1.4",
+    version="10.1.5",
     author="Yves Auad",
     description="Set of tools to run VG Cold Microscope in Nionswift",
     url="https://github.com/yvesauad/swift_lumiere",
